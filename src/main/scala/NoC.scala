7442a4d67361dc48701acdccd04a2664c439ceb8
Jerry Zhao <jerryz123@berkeley.edu>
2022-01-05 10:55:39 -0800

    Move Parameters contents to NoC

--- src/main/scala/NoC.scala
+++ src/main/scala/NoC.scala
@@ -4,8 +4,47 @@ import chisel3._
 import chisel3.util._
 
 import freechips.rocketchip.config.{Field, Parameters}
-import constellation.router.{Router, RouterParams}
+import constellation.router._
 import constellation.routing._
+
+
+case class NoCConfig(
+  nNodes: Int = 3,
+  flitPayloadBits: Int = 64,
+  maxFlits: Int = 8,
+  nVirtualNetworks: Int = 1,
+
+  // srcNodeId, destNodeId => virtualChannelParams
+  topology: (Int, Int) => Option[UserChannelParams] = (a: Int, b: Int) => None,
+  ingresses: Seq[UserIngressParams] = Nil,
+  egresses: Seq[UserEgressParams] = Nil,
+  routingRelation: RoutingRelation = RoutingRelations.allLegal,
+  routerParams: Int => UserRouterParams = (i: Int) => UserRouterParams(),
+  // (blocker, blockee) => bool
+  // If true, then blocker must be able to proceed when blockee is blocked
+  vNetBlocking: (Int, Int) => Boolean = (_: Int, _: Int) => true,
+)
+case object NoCKey extends Field[NoCConfig](NoCConfig())
+
+trait HasNoCParams {
+  implicit val p: Parameters
+  private val params = p(NoCKey)
+
+  val nNodes = params.nNodes
+  val maxFlits = params.maxFlits
+  val nVirtualNetworks = params.nVirtualNetworks
+
+  val flitPayloadBits = params.flitPayloadBits
+  val nodeIdBits = log2Ceil(params.nNodes)
+  val flitIdBits = log2Up(params.maxFlits+1)
+  val vNetBits = log2Up(params.nVirtualNetworks)
+  val nEgresses = params.egresses.size
+  val egressIdBits = log2Up(params.egresses.size)
+  val egressSrcIds = params.egresses.map(_.srcId)
+
+  val topologyFunction = params.topology
+  val routerParams = params.routerParams
+}
 
 class NoC(implicit val p: Parameters) extends Module with HasNoCParams{
   var uniqueChannelId = 0
