566e3e70bce0df17afca69e735f7f91b347c2088
Animesh Agrawal <animesha@berkeley.edu>
2022-04-19 11:23:46 -0700

    Rename IngressUnit to InstrumentationUnit

--- src/main/resources/csrc/InstrumentationUnit.cpp
+++ src/main/resources/csrc/InstrumentationUnit.cpp
@@ -2,8 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstring>
+#include <random>
+
+// https://github.com/ucb-bar/testchipip/blob/03535f56a6318236ab6abf5342d78eecf453984d/src/main/resources/testchipip/csrc/SimSerial.cc
+#include <vpi_user.h>
+#include <svdpi.h>
 
 using namespace std;
+
+char* TRAFFIC_MATRIX_PLUSARG = "+TRAFFIC_MATRIX="
 
 unsigned long long NUM_FLITS;
 unsigned long long NUM_INGRESSES;
@@ -17,20 +25,59 @@ struct flit {
 };
 
 /* Source Queues for input into NoC */
-vector<vector<struct flit>> *srcQueues;
+vector<vector<struct flit>> *SRC_QUEUES;
+
+/* TRAFFIC_MATRIX[i][e] specifies the flow rate from ingress i to egress e. */
+int **TRAFFIC_MATRIX
 
 /*
  * Called at the beginning of simulation. Used to initialize global state.
  */
 extern "C" void ingressunit_init(
   unsigned long long num_flits,
-  unsigned long long num_ingresses
+  unsigned long long num_ingresses,
+  unsigned long long num_egresses
 )
 {
+  // Initialize global state
   NUM_FLITS = num_flits;
   NUM_INGRESSES = num_ingresses;
 
-  srcQueues = new vector<vector<flit>>(NUM_INGRESSES, vector<flit>());
+  SRC_QUEUES = new vector<vector<flit>>(NUM_INGRESSES, vector<flit>());
+  TRAFFIC_MATRIX = new *int[num_ingresses];
+  for (int i = 0; i < num_ingresses; i++) {
+    TRAFFIC_MATRIX[i] = new int[num_egresses];
+  }
+
+  // Fill flow rate matrix
+  s_vpi_vlog_info info;
+  if (!vpi_get_vlog_info(&info)) {
+    printf("C++ sim: Unable to get plusargs from simulator\n");
+    abort();
+  }
+
+  char* filepath = NULL;
+  for (int i = 0; i < info.argc; i++) {
+    char* input_arg = info.argv[i];
+    if (strncmp(input_arg, TRAFFIC_MATRIX_PLUSARG) != NULL) {
+      filepath = input_arg + strlen(TRAFFIC_MATRIX_PLUSARG);
+    }
+  }
+  if (filepath == NULL) {
+    printf("C++ Sim: Unable to find traffic matrix filepath plusarg\n");
+  }
+
+  FILE* traffic_matrix_file = fopen(filepath, "r")
+  while (feof(traffic_matrix_file) != 0) {
+    int ingress; int egress; int flow_rate;
+    fscanf(file, " "); // skip leading whitespace
+    int num_matches = fscanf(traffic_matrix_file, "%d %d %d", &ingress, &egress, &flow_rate);
+    if (num_matches != 3) {
+      printf("C++ Sim: failed to read traffic matrix file (incorrect format)")
+    }
+    TRAFFIC_MATRIX[ingress][egress] = flow_rate;
+  }
+  fclose(traffic_matrix_file);
 }
 
 /* Given an input ingress, returns the egressID of the egress to send a packet to
@@ -70,14 +117,14 @@ extern "C" void ingressunit_tick(
       new_flit->tail = i == (NUM_FLITS - 1);
       new_flit->egress_id = dest;
       new_flit->payload = cycle_count;
-      (*srcQueues)[ingress_id].push_back(*new_flit);
+      (*SRC_QUEUES)[ingress_id].push_back(*new_flit);
     }
   }
 
-  bool flit_available = (*srcQueues)[ingress_id].size() > 0;
+  bool flit_available = (*SRC_QUEUES)[ingress_id].size() > 0;
   if (flit_available) {
     *flit_out_valid = 1;
-    flit& next_flit = (*srcQueues)[ingress_id].at(0);
+    flit& next_flit = (*SRC_QUEUES)[ingress_id].at(0);
     *flit_out_head = next_flit.head;
     *flit_out_tail = next_flit.tail;
     *flit_out_egress_id = next_flit.egress_id;
@@ -87,7 +134,7 @@ extern "C" void ingressunit_tick(
   }
 
   if (noc_ready != 0 && flit_available) {
-    (*srcQueues)[ingress_id].erase((*srcQueues)[ingress_id].begin());
+    (*SRC_QUEUES)[ingress_id].erase((*SRC_QUEUES)[ingress_id].begin());
   }
 
   return;
--- src/main/resources/vsrc/IngressUnit.v
+++ src/main/resources/vsrc/IngressUnit.v
@@ -2,16 +2,21 @@
 
 import "DPI-C" function void ingressunit_init(
     input longint unsigned      num_flits,
-    input longint unsigned      num_ingresses
+    input longint unsigned      num_ingresses,
+    input longint unsigned      num_egresses
 );
 
 import "DPI-C" function void ingressunit_tick
 (
     input longint unsigned      ingress_id,
     input longint unsigned      cycle_count,
-    input                       noc_ready, // TODO
-    output                      flit_bits, // TODO
-    output                      flit_valid, // TODO
+    input bit                   noc_ready,
+
+    output bit                  flit_out_valid,
+    output bit                  flit_out_head,
+    output bit                  flit_out_tail,
+    output longint unsigned     flit_out_egress_id,
+    output longint unsigned     flit_out_payload
     // see InputGen in TestHarness.scala for how IngressFlit are generated
     // probably easier to have verilog/C++ return IngressFlit bits and then
     // actually construct IngressFlit in Chisel using those values
@@ -19,12 +24,13 @@ import "DPI-C" function void ingressunit_tick
 
 module IngressUnit #(
     NUM_INGRESSES = 1,
+    NUM_EGRESSES = 1,
     INGRESS_ID = 0,
     INJECTION_RATE = 100,
     NUM_FLITS = 1
 ) (
     input clock,
-    input reset
+    input reset,
 );
     initial begin
         if (INGRESS_ID == 0) begin
