7ce404a2909a470cfbb08ebcf4e0a1a1d8e23c77
Jerry Zhao <jerryz123@berkeley.edu>
2022-04-26 18:35:12 -0700

    Add stall prob to TestConfig20

--- src/main/scala/test/Configs.scala
+++ src/main/scala/test/Configs.scala
@@ -173,6 +173,7 @@ class TestConfig19 extends Config(
   new constellation.topology.WithTopology(new BidirectionalTorus1D(10)))
 
 class TestConfig20 extends Config(
+  new WithInputStallProbability(0.8) ++
   new constellation.channel.WithUniformNVirtualChannels(4, UserVirtualChannelParams(5)) ++
   new constellation.channel.WithFullyConnectedIngresses ++
   new constellation.channel.WithIngresses((0 until 10)) ++
