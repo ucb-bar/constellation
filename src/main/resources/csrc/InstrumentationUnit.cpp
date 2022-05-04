b4834dfd6355f56de8b8434efe41772d3b391158
Animesh Agrawal <animesha@berkeley.edu>
2022-05-03 17:07:23 -0700

    Add latency to measurements

--- src/main/resources/csrc/InstrumentationUnit.cpp
+++ src/main/resources/csrc/InstrumentationUnit.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -45,9 +46,9 @@ unsigned long long NUM_EGRESSES;
 typedef struct flit {
   bool head;
   bool tail;
-  unsigned long long ingress_id; // src
-  unsigned long long egress_id; // dst
-  unsigned long long payload; // cycle_time flit was created
+  unsigned long long ingress_id;
+  unsigned long long egress_id;
+  unsigned long long payload;
 } flit;
 
 /* Source Queues for input into NoC */
@@ -56,66 +57,87 @@ vector<queue<struct flit*>>* SRC_QUEUES;
 /* All packets currently in-flight in the NoC. keys: flit unique id, values: destination egress id */
 map<long, unsigned long long>* FLITS_IN_FLIGHT;
 
-// todo: Document
+/* flit_out_valid[ingress_id] is the ingress's valid signal from the previous cycle. The output of
+ * ingressunit_tick is delayed by one cycle, so the current cycle's noc_ready must be compared with
+ * the previous cycle's flit_out_valid to determine if a flit was actually sent into the network.
+ */
 char* last_flit_out_valid;
 
-/* Returns true if cycle cycle_count is in the warmup phase. */
+/* Random number generator for generating packets to inject. */
+unsigned int seed = 152; // std::chrono::system_clock::now().time_since_epoch().count();
+std::default_random_engine generator (seed);
+uniform_real_distribution<float> egress_selector(0.0, 1.0);
+
+/* True if simulation metrics have been printed out. Since all egress units are simulated by the
+ * same function, this prevents the results from being printed multiple times.
+ */
+bool METRICS_PRINTED = false;
+
+/* CSV of packet latencies. */
+FILE* LATENCY_CSV = NULL;
+
+/* Latency of every measurement packet. */
+vector<long>* LATENCIES;
+
+/* Given the current CYCLE_COUNT, returns true if simulation is in the warmup phase. */
 bool IS_WARMUP(unsigned long long cycle_count) {
   if (SIM_START < 0) {
-    printf("C++ Sim: SIM_START not set\n");
+    printf("C++ Sim: SIM_START not set before call to IS_WARMUP. Aborting.\n");
     abort();
   }
   unsigned long long adjusted_cnt = cycle_count - SIM_START;
   return adjusted_cnt <= WARMUP_CYCLES;
 }
 
-/* Returns true if cycle cycle_count is in the measurement phase. */
+/* Given the current CYCLE_COUNT, returns true if simulation is in the measurement phase. */
 bool IS_MEASUREMENT(unsigned long long cycle_count) {
   if (SIM_START < 0) {
-    printf("C++ Sim: SIM_START not set\n");
+    printf("C++ Sim: SIM_START not set before call to IS_MEASUREMENT. Aborting.\n");
     abort();
   }
   unsigned long long adjusted_cnt = cycle_count - SIM_START;
   return (adjusted_cnt > WARMUP_CYCLES) && (adjusted_cnt <= (WARMUP_CYCLES + MEASUREMENT_CYCLES));
 }
 
-/* Returns true if cycle cycle_count is in the drain phase. */
+/* Given the current CYCLE_COUNT, returns true if simulation is in the drain phase. */
 bool IS_DRAIN(unsigned long long cycle_count) {
   if (SIM_START < 0) {
-    printf("C++ Sim: SIM_START not set\n");
+    printf("C++ Sim: SIM_START not set before call to IS_DRAIN. Aborting.\n");
     abort();
   }
   unsigned long long adjusted_cnt = cycle_count - SIM_START;
   return adjusted_cnt > (WARMUP_CYCLES + MEASUREMENT_CYCLES);
 }
 
-/* Returns true if cycle cycle_count has exceeded the maximum time allowed for the drain phase. */
+/* Returns true if the simulation has exceeded the maximum time allowed for the drain phase. */
 bool IS_DRAIN_TIMEOUT(unsigned long long cycle_count) {
   if (SIM_START < 0) {
-    printf("C++ Sim: SIM_START not set\n");
+    printf("C++ Sim: SIM_START not set before call to IS_DRAIN_TIMEOUT. Aborting.\n");
     abort();
   }
   unsigned long long adjusted_cnt = cycle_count - SIM_START;
   return adjusted_cnt > (WARMUP_CYCLES + MEASUREMENT_CYCLES + DRAIN_TIMEOUT);
 }
 
+/* Generates a unique ID for each flit. */
 long _UNIQ_ID = 0;
 long gen_unique_id_flit() {
   _UNIQ_ID = _UNIQ_ID + 1;
   return _UNIQ_ID;
 }
 
-/* Generates the payload for a measurement flit. */
+/* Generates the payload for a measurement flit. The LSBs of the payload stores the cycle the flit
+ * was created and the MSBs store the flit ID. */
 unsigned long long gen_flit_payload(unsigned long long cycle_count) {
   return (gen_unique_id_flit() << 32) | (0xFFFFFFFF & cycle_count);
 }
 
-/* Given a flit's payload, returns the flit's unique id (use only on measurement flits). */
+/* Given a measurement flit's payload, returns the flit's unique id. */
 unsigned long get_unique_id_flit(unsigned long long payload) {
   return (payload >> 32);
 }
 
-/* Given a flit's payload, returns the cycle the flit was generated (use only on measurement flits). */
+/* Given a measurement flit's payload, returns the cycle the flit was generated. */
 unsigned long get_cyc_flit(unsigned long long payload) {
   return payload & 0xFFFFFFFF;
 }
@@ -146,10 +168,14 @@ extern "C" void instrumentationunit_init(
     }
   }
 
+  LATENCY_CSV = fopen("noc-packet-latencies.csv", "w");
+  fprintf(LATENCY_CSV, "ingress,egress,latency\n");
+  LATENCIES = new vector<long>();
+
   // Fill flow rate matrix
   s_vpi_vlog_info info;
   if (!vpi_get_vlog_info(&info)) {
-    printf("C++ sim: Unable to get plusargs from simulator\n");
+    printf("C++ sim: Unable to get plusargs from simulator. Aborting.\n");
     abort();
   }
   char* filepath = NULL;
@@ -157,23 +183,22 @@ extern "C" void instrumentationunit_init(
     char* input_arg = info.argv[i];
     if (strncmp(input_arg, TRAFFIC_MATRIX_PLUSARG, strlen(TRAFFIC_MATRIX_PLUSARG)) == 0) {
       filepath = input_arg + strlen(TRAFFIC_MATRIX_PLUSARG);
-      printf("C++ Sim: traffic matrix filepath is %s\n", filepath);
       break;
     }
   }
   if (filepath == NULL) {
-    printf("C++ Sim: Unable to find traffic matrix filepath plusarg\n");
+    printf("C++ Sim: Unable to find traffic matrix filepath plusarg. Aborting.\n");
     abort();
   }
   FILE* traffic_matrix_file = fopen(filepath, "r");
   if (traffic_matrix_file == NULL) {
-    printf("C++ Sim: Unable to open traffic matrix file\n");
+    printf("C++ Sim: Unable to open traffic matrix file. Aborting.\n");
     abort();
   }
   fscanf(traffic_matrix_file, " "); // skip leading whitespace
   int num_matches = fscanf(traffic_matrix_file, "%d %d %d %d", &NUM_FLITS, &WARMUP_CYCLES, &MEASUREMENT_CYCLES, &DRAIN_TIMEOUT);
   if (num_matches != 4) {
-      printf("C++ Sim: failed to read config options from traffic matrix file. Read only %d args.\n", num_matches);
+      printf("C++ Sim: failed to read config options from traffic matrix file. Read only %d args. Aborting.\n", num_matches);
       abort();
   }
 
@@ -205,20 +230,13 @@ extern "C" void instrumentationunit_init(
       sum += ingress_routing[e];
     }
     if (sum > 1.0) {
-      printf("C++ Sim: Invalid traffic matrix (each row must sum to <= 1.0)\n");
+      printf("C++ Sim: Invalid traffic matrix (each row must sum to <= 1.0). Aborting.\n");
       abort();
     }
   }
 
-  printf("DEBUG: C++ sim initialized\n");
-
   return;
 }
-
-// TODO: MAKE SEED PARAMETERIZABLE (OR REQUEST RANDOM SEED)
-unsigned int seed = 152; // std::chrono::system_clock::now().time_since_epoch().count();
-std::default_random_engine generator (seed);
-uniform_real_distribution<float> egress_selector(0.0, 1.0);
 
 /* Given an input ingress, returns the egressID of the egress to send a packet to
  * at this cycle. No packet should be sent if the output is -1, otherwise cast the output to
@@ -228,7 +246,6 @@ long long gen_packet(
   unsigned long long ingress_id,
   unsigned long long cycle_count
 ) {
-
   float selected_ingress_sample = egress_selector(generator);
 
   float* ingress_traffic_pattern = TRAFFIC_MATRIX[ingress_id];
@@ -258,11 +275,9 @@ extern "C" void ingressunit_tick(
   unsigned long long* flit_out_payload
 ) {
   if (noc_ready != 0 && SIM_START == (unsigned long long) -1) {
-    printf("C++ Sim: Simulation starting at cycle %llu\n", cycle_count);
+    printf("C++ Sim: Simulation starting at cycle %llu.\n", cycle_count);
     SIM_START = cycle_count;
   }
-
-  // printf("C++ Sim DEBUG: ingressunit %d at cycle %llu\n", ingress_id, cycle_count);
 
   unsigned long long dest = gen_packet(ingress_id, cycle_count);
   if (((long long) dest) != -1) {
@@ -281,10 +296,9 @@ extern "C" void ingressunit_tick(
     }
   }
 
-  // TODO (ANIMESH):
   if (noc_ready != 0 && last_flit_out_valid[ingress_id] != 0) {
     if ((*SRC_QUEUES)[ingress_id].size() == 0) {
-      printf("C++ Sim: Tried to pop from an empty source queue.\n");
+      printf("C++ Sim: Ingress %llu tried to pop from an empty source queue at cycle %llu. Aborting.\n", ingress_id, cycle_count);
       abort();
     }
     flit* last_flit = (*SRC_QUEUES)[ingress_id].front();
@@ -292,20 +306,12 @@ extern "C" void ingressunit_tick(
 
     if (last_flit->payload != (unsigned long long) -1) {
       (*FLITS_IN_FLIGHT).insert(pair<long, unsigned long long>(get_unique_id_flit(last_flit->payload), last_flit->egress_id));
-      printf("DEBUG: Inserting flit %ld to egress %llu at cycle %llu\n", get_unique_id_flit(last_flit->payload), last_flit->egress_id, cycle_count);
-      printf("\tFLITS_IN_FLIGHT now size %d\n", (*FLITS_IN_FLIGHT).size());
-    } else {
-      // printf("DEBUG: Inserting flit W to egress %llu at cycle %llu\n", next_flit->egress_id, cycle_count);
     }
-    delete last_flit; // no need to track warmup/drain flits
+    delete last_flit;
   }
 
   bool flit_available = (*SRC_QUEUES)[ingress_id].size() > 0;
 
-  // TODO (ANIMESH): THE VALUES HERE ARE SENT OUT THE CYCLE AFTER THIS ONE.
-  // What the logic above does: If we outputted a valid signal last cycle, its visible this cycle.
-  // If we also see a ready cycle this signal, that means both ready and valid were high so we should
-  // remove a flit from the queue
   flit* next_flit = NULL;
   if (flit_available) {
     next_flit = (*SRC_QUEUES)[ingress_id].front();
@@ -324,8 +330,6 @@ extern "C" void ingressunit_tick(
   return;
 }
 
-bool METRICS_PRINTED = false;
-
 double compute_throughput() {
   // expected traffic from ingress i to egress e is TRAFFIC_MATRIX[i][e] * MEASUREMENT_CYCLES
   // actual traffic is PACKETS_RECVD_M
@@ -342,7 +346,33 @@ double compute_throughput() {
     }
   }
   fclose(csv);
+  printf("C++ Sim: THROUGHPUT IS %f\n", min_throughput);
   return min_throughput;
+}
+
+void compute_latency(double throughput) {
+  if ((*LATENCIES).size() <= 0) {
+    printf("Attempting to compute latency with 0 packets in latency vector. Aborting.\n");
+    abort();
+  }
+
+  if (throughput < 1.0) {
+    printf("C++ Sim: Latency not displayed since latency is infinite when throughput is less than 1.0\n");
+    return;
+  }
+  fclose(LATENCY_CSV);
+  sort((*LATENCIES).begin(), (*LATENCIES).end());
+  printf("C++ Sim: Minimum latency was %ld cycles\n", (*LATENCIES)[0]);
+  if ((*LATENCIES).size() % 2 == 0) {
+    long sample1 = (*LATENCIES)[(*LATENCIES).size() / 2];
+    long sample2 = (*LATENCIES)[((*LATENCIES).size() / 2) + 1];
+    double median = ((double) (sample1 + sample2)) / 2.0;
+    printf("C++ Sim: Median latency was %f cycles\n", median);
+  } else {
+    printf("C++ Sim: Median latency was %ld cycles\n", (*LATENCIES)[(*LATENCIES).size() / 2]);
+  }
+  printf("C++ Sim: Maximum latency was %ld cycles\n", (*LATENCIES)[(*LATENCIES).size() - 1]);
+  return;
 }
 
 extern "C" void egressunit_tick(
@@ -361,9 +391,9 @@ extern "C" void egressunit_tick(
   *egressunit_ready = 1;
 
   if (IS_DRAIN_TIMEOUT(cycle_count) && !METRICS_PRINTED) {
-    printf("C++ Sim: Ending simulation at cycle %llu\n", cycle_count);
-    printf("C++ Sim: Simulation timed out (drain took too long). Ending so you can view a waveform.\n", cycle_count);
-    printf("C++ Sim: THROUGHPUT IS %f\n", compute_throughput());
+    printf("C++ Sim: Simulation timed out at cycle %llu (drain took too long). Ending so you can view a waveform.\n", cycle_count);
+    printf("C++ Sim: Latency not displayed since drain was interrupted. Do not use the values in the latency CSV.\n");
+    compute_throughput();
     METRICS_PRINTED = true;
     *success = 1;
     return;
@@ -380,14 +410,23 @@ extern "C" void egressunit_tick(
 
   if (received_flit.payload != (unsigned long long) -1) {
     int num_erased = (*FLITS_IN_FLIGHT).erase(get_unique_id_flit(received_flit.payload));
-    printf("DEBUG: egress %llu received flit %ld at cycle %llu\n", egress_id, get_unique_id_flit(received_flit.payload), cycle_count);
-    printf("\tDEBUG: erased %d from set. %d left.\n", num_erased, (*FLITS_IN_FLIGHT).size());
-  } else {
-    // printf("DEBUG: egress %llu received W at cycle %llu\n", egress_id, cycle_count);
+    if (num_erased <= 0) {
+      printf("C++ Sim: Received a measurement flit that wasn't being tracked. Aborting. \n");
+      abort();
+    }
   }
 
-  if (received_flit.tail != 0 && received_flit.payload != (unsigned long long) -1 && IS_MEASUREMENT(cycle_count)) {
+  if (received_flit.tail != 0 && (received_flit.payload != (unsigned long long) -1) && IS_MEASUREMENT(cycle_count)) {
     PACKETS_RECVD_M[received_flit.ingress_id][received_flit.egress_id]++;
+  }
+
+  if (received_flit.tail != 0 && (received_flit.payload != (unsigned long long) -1)) {
+    // latency is calculated by t_{last bit received} - t_{first bit generated}. Normally the latter refers
+    // to the cycle the head flit is generated, but since the C++ sim generates all flits in the same
+    // cycle, we can use the cycle the tail flit is generated.
+    long latency = cycle_count - get_cyc_flit(received_flit.payload);
+    fprintf(LATENCY_CSV, "%llu,%llu,%lu\n", received_flit.ingress_id, received_flit.egress_id, latency);
+    (*LATENCIES).push_back(latency);
   }
 
   if ((*FLITS_IN_FLIGHT).size() != 0 || !IS_DRAIN(cycle_count)) {
@@ -397,10 +436,11 @@ extern "C" void egressunit_tick(
 
   *success = 1;
 
-  /* Compute and print out metrics. (TODO: LATENCY) */
+  /* Compute and print out metrics. */
   if (!METRICS_PRINTED) {
     printf("C++ Sim: Ending simulation at cycle %llu\n", cycle_count);
-    printf("C++ Sim: THROUGHPUT IS %f\n", compute_throughput());
+    long throughput = compute_throughput();
+    compute_latency(throughput);
     METRICS_PRINTED = true;
   }
 
