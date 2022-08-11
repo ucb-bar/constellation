af41e86944576a1ca9b7e86c5836904d9bd29448
Jerry Zhao <jerryz123@berkeley.edu>
2022-08-11 00:21:12 -0700

    Flatten truth tables into a single large table

--- src/main/scala/router/RouteComputer.scala
+++ src/main/scala/router/RouteComputer.scala
@@ -53,47 +53,49 @@ class RouteComputer(
       assert(!req.valid)
       resp.bits.vc_sel := DontCare
     } else {
+
+      def tupleFlatten(t: (Int, Int, Int, Int)): UInt = {
+        val l2 = (BigInt(t._1) << ingressIdBits) | t._2
+        val l3 = (l2 << egressIdBits) | t._3
+        val width = req.bits.src_virt_id.getWidth + ingressIdBits + egressIdBits + log2Ceil(nNodes)
+        ((l3 << log2Ceil(nNodes)) | t._4).U(width.W)
+      }
+
       val flow = req.bits.flow
-      (0 until nAllOutputs).map { o =>
-        if (o < nOutputs) {
-          (0 until outParams(o).nVirtualChannels).map { outVId =>
-            val table = allInParams(i).possibleFlows.toSeq.distinct.map { pI =>
-              allInParams(i).channelRoutingInfos.map { cI =>
-                val v = routingRelation(
-                  cI,
-                  outParams(o).channelRoutingInfos(outVId),
-                  pI
-                )
-                ((cI.vc, pI.ingressId, pI.egressId, pI.dst), v)
-              }
-            }.flatten
-            val trues = table.filter(_._2).map(_._1)
-            val falses = table.filter(!_._2).map(_._1)
 
-            val vcWidth = req.bits.src_virt_id.getWidth
-            def tupleFlatten(t: (Int, Int, Int, Int)): UInt = {
-              val l2 = (BigInt(t._1) << ingressIdBits) | t._2
-              val l3 = (l2 << egressIdBits) | t._3
-              ((l3 << log2Ceil(nNodes)) | t._4).U
+      val table = allInParams(i).possibleFlows.toSeq.distinct.map { pI =>
+        allInParams(i).channelRoutingInfos.map { cI =>
+          var row: String = "b"
+          (0 until nOutputs).foreach { o =>
+            (0 until outParams(o).nVirtualChannels).foreach { outVId =>
+              row = row + (if (routingRelation(cI, outParams(o).channelRoutingInfos(outVId), pI)) "1" else "0")
             }
+          }
+          ((cI.vc, pI.ingressId, pI.egressId, pI.dst), row)
+        }
+      }.flatten
+      val addr = Cat(
+        req.bits.src_virt_id,
+        flow.ingress_id,
+        flow.egress_id,
+        flow.egress_dst_id)
+      val width = outParams.map(_.nVirtualChannels).reduce(_+_)
+      val decoded = if (table.size > 0) {
+        val truthTable = TruthTable(
+          table.map { e => (BitPat(tupleFlatten(e._1)), BitPat(e._2)) },
+          BitPat("b" + "?" * width)
+        )
+        Reverse(decoder(addr, truthTable))
+      } else {
+        0.U(width.W)
+      }
+      var idx = 0
 
-            resp.bits.vc_sel(o)(outVId) := (if (falses.size == 0) {
-              true.B
-            } else if (trues.size == 0) {
-              false.B
-            } else {
-              val truthTable = TruthTable(
-                (trues.map  { t => (BitPat(tupleFlatten(t)), BitPat(true.B)) } ++
-                 falses.map { t => (BitPat(tupleFlatten(t)), BitPat(false.B)) }),
-                BitPat("b?")
-              )
-              val addr = Cat(
-                req.bits.src_virt_id,
-                flow.ingress_id,
-                flow.egress_id,
-                flow.egress_dst_id)
-              decoder(addr, truthTable)
-            })
+      (0 until nAllOutputs).foreach { o =>
+        if (o < nOutputs) {
+          (0 until outParams(o).nVirtualChannels).foreach { outVId =>
+            resp.bits.vc_sel(o)(outVId) := decoded(idx)
+            idx += 1
           }
         } else {
           resp.bits.vc_sel(o)(0) := false.B
